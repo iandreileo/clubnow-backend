baf06a474b294a299b3768437c7f40421aca342c
Catalin-P <catalin.picior00@gmail.com>
2022-05-11 02:44:11 +0300

    create Firebase  connection

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+// init app with given firebase  config
+const firebaseConfig = {
+    apiKey: "AIzaSyCy8DjSteCjsXxdw52uKn2SkGjaGI3PREo",
+    authDomain: "clubnow-2f488.firebaseapp.com",
+    projectId: "clubnow-2f488",
+    storageBucket: "clubnow-2f488.appspot.com",
+    messagingSenderId: "132050914827",
+    appId: "1:132050914827:web:71e4d5cce673ac0125010e"
+};
+
+const app = initializeApp(firebaseConfig);
